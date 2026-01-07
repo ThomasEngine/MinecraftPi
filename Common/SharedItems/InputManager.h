bfa07e4f8fce791870c5c67ba80ddbee0cc99565
ThomasEngine <thomas.van.mildert@gmail.com>
2026-01-07 18:31:14 +0100

    Finnaly compiling on pi

--- Common/SharedItems/InputManager.h
+++ Common/SharedItems/InputManager.h
@@ -17,14 +17,14 @@ public:
     void BindAction(MouseButtons button, const std::string& actionName);
     void BindCommand(const std::string& actionName, std::unique_ptr<ICommand> command);
     bool IsKeyboardActionActive(const std::string& actionName) const;
-	bool IsMouseActionActive(const std::string& actionName) const;
+    bool IsMouseActionActive(const std::string& actionName) const;
     void ProcessCommands(Player& player, float deltaTime);
 
 private:
     const Input* m_Input;
     std::map<Key, std::string> m_KeyToAction;
     std::map<std::string, std::unique_ptr<ICommand>> m_ActionToCommand;
-	std::map<MouseButtons, std::string> m_MouseButtonToAction;
+    std::map<MouseButtons, std::string> m_MouseButtonToAction;
     std::map<std::string, bool> m_MouseActionStates;
     std::map<std::string, bool> m_KeyboardActionStates;
 };
